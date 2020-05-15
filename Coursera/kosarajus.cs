5ba724ded47a049ff09ec576bcbe4134e97b7cf9
Kai Sun <bryant.kai1227@gmail.com>
2020-05-15 14:39:03 -0400

    finsihing Q1 week1, SCC size

--- Coursera/kosarajus.cs
+++ Coursera/kosarajus.cs
@@ -3,11 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace Coursera{
     public class kosarajus {
 
+        /*
+        
+        The file contains the edges of a directed graph. Vertices are labeled as positive integers from 1 to 875714. 
+        Every row indicates an edge, the vertex label in first column is the tail and the vertex label in second column is the head (recall the graph is directed, and the edges are directed from the first column vertex to the second column vertex). So for example, the 11th
+        row looks liks : "2 47646". This just means that the vertex with label 2 has an outgoing edge to the vertex with label 47646
+        Your task is to code up the algorithm from the video lectures for computing strongly connected components (SCCs), and to run this algorithm on the given graph.
+        
+        Output Format: You should output the sizes of the 5 largest SCCs in the given graph, in decreasing order of sizes, separated by commas (avoid any spaces). 
+        So if your algorithm computes the sizes of the five largest SCCs to be 500, 400, 300, 200 and 100, then your answer should be "500,400,300,200,100"
+
+
+        WARNING: This is the most challenging programming assignment of the course. Because of the size of the graph you may have to manage memory carefully. The best way to do this depends on your programming language and environment,
+         and we strongly suggest that you exchange tips for doing this on the discussion forums.
+        */
+        
         public int kosarajus_twopass(){
             var textFile = "./SCC.txt";
             string[] lines = File.ReadAllLines(textFile);
@@ -76,22 +90,40 @@ namespace Coursera{
             GC.Collect();
 
             //------------------------------------------------------------------------------------------------------------------------------------------------
-            
+            //I am using Stack data structure to run DFS because if i do recursion instead of Stack, program will run into stackoverflow due to high number of recursion occuring during the executaiton
+            // Stackoverflow is an error using too much memory, like too many recursion, taking up stack (memory) size, there could be a way in C# to increase the stack size, but using while loop is better 
+            // way to avoid extra memory and run faster, so i stop to use stack data sctructure 
+            // We dont need to worry about the finsihing time  order, beacuse we are finding SCC group, no matter what's the order inside a SCC, the largest finishing time in a SCC is always the same value, so order does not matter,
+            // Tha's why we dont have to use recursion dfs to implement our question
+
             DFS_Loop_reverse(list_arr,list_arr_reverse,explore, finishing_time, max);
             for(int i = 0;i < max; ++i){
                 explore[i] = 0;
             }
 
-            for(int i = 0;i < finishing_time.Count; ++i){
-                if (finishing_time[i] == 875709){
-                    continue;
+            var ans = DFS_Loop(list_arr, explore, finishing_time, max);
+            ans.Sort(); // sort the list of size of SCC
+
+
+            int one = 0;
+            int two = 0;
+            int three = 0;
+            int four = 0;
+            int five = 0;
+            for(int j = 0;j < ans.Count;++j){
+                if(ans[j] >= one){
+                    five = four;
+                    four = three;
+                    three = two;
+                    two = one;
+                    one = ans[j];
                 }
             }
-            var ans = DFS_Loop(list_arr, explore, finishing_time, max);
             return 0;
                 
         }
 
+         //Stack DFS on reverse order graph
         public void DFS_Loop_reverse(List<Dictionary<string,List<string>>> list_arr,List<Dictionary<string,List<string>>> list_arr_reverse,List<int> explore, List<int> finishing_time, int max){
             for(int i = max-1;i > -1; --i){
                 if(explore[i] == 0){
@@ -103,6 +135,7 @@ namespace Coursera{
             }
         }
 
+        // DFS: first time running through the graph, assigning finsih time to each vertex, since it is using stack the first one been vistied will always has to large finsihing time.
         public void DFS(List<Dictionary<string,List<string>>> list_arr,List<Dictionary<string,List<string>>> list_arr_reverse,List<int> explore, List<int> finishing_time, int node_num, Stack<int> stack){
             Stack<int> visited = new Stack<int>();
             int stack_num = 1;
@@ -124,18 +157,17 @@ namespace Coursera{
                 visit_num ++;
             }
 
-            if(stack_num != visit_num){
-                var x = 0;
-            }
             var visit_count = visited.Count; // must have this line, C# for loop 中间的condiction 是 dynamic的， 如果下面的forloop 我写j < visited.Count， 因为随着我们每次visited的pop
             // 会减小visited的size ， 因为C#的 for loop 每一遍都会重新运行 一次visited.count 所以到最后的结局就是， visited.count每一次都会自动减小，然后我们就read不全我们的data， 我他妈刚才在这儿 一脸懵逼找bug找了1个多小时
             // ！！！ 艹！！！！！！！
+
             for(int j = 0;j < visit_count; ++j){
                 var pop = visited.Pop();
                 finishing_time.Add(pop);
             }
         }
 
+        // Stack DFS on normal directrion graph
         public List<int> DFS_Loop(List<Dictionary<string,List<string>>> list_arr, List<int> explore, List<int> finishing_time, int max){
             List<int> SCC = new List<int>();
             for(int i = max-1;i > -1; --i){
@@ -150,6 +182,8 @@ namespace Coursera{
             return SCC;
         }
 
+        // DFS_Count(): Returns a list of size of SCC from VIsited.Count
+        // Visited stores all vertex in one SCC
          public int DFS_Count(List<Dictionary<string,List<string>>> list_arr, List<int> explore, List<int> finishing_time, int node_num, Stack<int> stack){
             Stack<int> visited = new Stack<int>();
             while(true){
@@ -175,7 +209,7 @@ namespace Coursera{
 
 
 
-// Recursion is causing stackoverflow
+        // Recursion is causing stackoverflow, so i abondoned it
         // public int DFS_Loop_reverse(List<Dictionary<string,List<string>>> list_arr, List<Dictionary<string,List<string>>> list_arr_reverse, List<int> explore, List<int> finishing_time, int max){
         //     // i will push index to finishing_time , this case, the one with lowest finishing will be positioned at the back of the finishing_time
         //     for (int i = max-1;i > -1; --i){
